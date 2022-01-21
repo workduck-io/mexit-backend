c815bc6e819fb57018fc99571750736e26f6b692
Rishivikram N <rishi.vikram.1@gmail.com>
2022-01-21 21:45:25 +0530

    Added tokenvalidator middleware and removed the workspace and namespace entity

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,16 +1,11 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import { NodeManager } from './managers/NodeManager';
-import { WorkspaceManager } from './managers/WorkspaceManager';
 import { AuthManager } from './managers/AuthManager';
 
 const container = new Container();
 
 container.bind<NodeManager>(NodeManager).to(NodeManager).inSingletonScope();
-container
-  .bind<WorkspaceManager>(WorkspaceManager)
-  .to(WorkspaceManager)
-  .inSingletonScope();
 container.bind<AuthManager>(AuthManager).to(AuthManager).inSingletonScope();
 
 export default container;
