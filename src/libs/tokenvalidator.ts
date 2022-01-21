f0da2cbcd53ea5132e6c1cd05f680a9649f15c69
Rishivikram N <rishi.vikram.1@gmail.com>
2022-01-21 21:45:25 +0530

    Added tokenvalidator middleware and removed the workspace and namespace entity

---
+++ src/libs/tokenvalidator.ts
@@ -0,0 +1,121 @@
+import { promisify } from 'util';
+import * as Axios from 'axios';
+import * as jsonwebtoken from 'jsonwebtoken';
+import jwkToPem from 'jwk-to-pem';
+import config from '../config';
+import { errorlib } from './errorlib';
+import { errorCodes } from './errorCodes';
+import { statusCodes } from './statusCodes';
+
+export interface ClaimVerifyRequest {
+  readonly token?: string;
+}
+
+export interface ClaimVerifyResult {
+  readonly userName: string;
+  readonly eventId: string;
+  readonly isValid: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  readonly error?: any;
+}
+
+interface TokenHeader {
+  kid: string;
+  alg: string;
+}
+interface PublicKey {
+  alg: string;
+  e: string;
+  kid: string;
+  kty: string;
+  n: string;
+  use: string;
+}
+interface PublicKeyMeta {
+  instance: PublicKey;
+  pem: string;
+}
+
+interface PublicKeys {
+  keys: PublicKey[];
+}
+
+interface MapOfKidToPublicKey {
+  [key: string]: PublicKeyMeta;
+}
+
+interface Claim {
+  token_use: string;
+  auth_time: number;
+  iss: string;
+  exp: number;
+  email: string;
+  event_id: string;
+}
+
+const cognitoPoolId = config.cognito.USER_POOL_ID || '';
+if (!cognitoPoolId) {
+  throw new Error('env var required for cognito pool');
+}
+const cognitoIssuer = `https://cognito-idp.us-east-1.amazonaws.com/${cognitoPoolId}`;
+
+let cacheKeys: MapOfKidToPublicKey | undefined;
+const getPublicKeys = async (): Promise<MapOfKidToPublicKey> => {
+  if (!cacheKeys) {
+    const url = `${cognitoIssuer}/.well-known/jwks.json`;
+    const publicKeys = await Axios.default.get<PublicKeys>(url);
+    cacheKeys = publicKeys.data.keys.reduce((agg, current) => {
+      const pem = jwkToPem(current);
+      agg[current.kid] = { instance: current, pem };
+      return agg;
+    }, {} as MapOfKidToPublicKey);
+    return cacheKeys;
+  } else {
+    return cacheKeys;
+  }
+};
+
+const verifyPromised = promisify(jsonwebtoken.verify.bind(jsonwebtoken));
+
+const TokenHandler = async (
+  request: ClaimVerifyRequest
+): Promise<ClaimVerifyResult> => {
+  let result: ClaimVerifyResult;
+  try {
+    const token = request.token;
+    const tokenSections = (token || '').split('.');
+    if (tokenSections.length < 2) {
+      throw new Error('requested token is invalid');
+    }
+
+    const headerJSON = Buffer.from(tokenSections[0], 'base64').toString('utf8');
+    const header = JSON.parse(headerJSON) as TokenHeader;
+    const keys = await getPublicKeys();
+    const key = keys[header.kid];
+    if (key === undefined) {
+      throw new Error('claim made for unknown kid');
+    }
+    const claim = (await verifyPromised(token, key.pem)) as Claim;
+    const currentSeconds = Math.floor(new Date().valueOf() / 1000);
+    if (currentSeconds > claim.exp || currentSeconds < claim.auth_time) {
+      throw new Error('claim is expired or invalid');
+    }
+    if (claim.iss !== cognitoIssuer) {
+      throw new Error('claim issuer is invalid');
+    }
+
+    if (claim.token_use !== 'id') {
+      throw new Error('claim use is not id');
+    }
+    result = {
+      userName: claim.email,
+      eventId: claim.event_id,
+      isValid: true,
+    };
+  } catch (error) {
+    result = { userName: '', eventId: '', error, isValid: false };
+  }
+  return result;
+};
+
+export { TokenHandler };
---
+++ src/middlewares/authrequest.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import { errorCodes } from '../libs/errorCodes';
+import { statusCodes } from '../libs/statusCodes';
+import { TokenHandler } from '../libs/tokenvalidator';
+// Authenticates the user for accessing
+// the endpoint routes.
+async function AuthRequest(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  // remove the 'Bearer ' token from the auth token
+  const token = req.headers.authorization?.replace('Bearer ', '');
+  try {
+    if (!token) throw new Error('Access denied. No token provided');
+
+    const result = await TokenHandler({ token });
+    if (result.isValid) {
+      next();
+    } else throw new Error('Invalid Token.');
+  } catch (error) {
+    res.status(statusCodes.FORBIDDEN).send({
+      message: error.message,
+      statusCode: statusCodes.FORBIDDEN,
+      errorCode: errorCodes.AUTH_ERROR,
+    });
+  }
+}
+
+export { AuthRequest };
