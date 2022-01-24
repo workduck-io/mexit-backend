3c2c27ca3c5c8ca556a111947bdd36c858dee790
Rishivikram N <rishi.vikram.1@gmail.com>
2022-01-24 14:52:54 +0530

    Added memoized functions for caching

--- src/libs/GotClientClass.ts
+++ src/libs/GotClientClass.ts
@@ -1,11 +1,14 @@
 import got, { OptionsOfTextResponseBody } from 'got';
 import { injectable } from 'inversify';
 import { GotResponse, GotClientType } from 'src/interfaces/GotClient';
-
+import StatsMap from 'stats-map';
+import mem from 'mem';
 @injectable()
 export class GotClient implements GotClientType {
   private _requestTimeout = 15000;
   private _gotResponse: GotResponse = { data: null, status: null };
+  private _cache = new StatsMap();
+  private _memGot = mem(got, { cache: this._cache });
 
   private _gotConfig: OptionsOfTextResponseBody = {
     hooks: {
@@ -32,7 +35,7 @@ export class GotClient implements GotClientType {
     payload: T,
     authToken: string
   ): Promise<GotResponse> {
-    this._gotResponse.data = await got
+    this._gotResponse.data = await this._memGot
       .post(url, {
         json: payload,
         headers: {
@@ -46,7 +49,7 @@ export class GotClient implements GotClientType {
   }
 
   async delete(url: string, authToken: string): Promise<GotResponse> {
-    this._gotResponse.data = await got
+    this._gotResponse.data = await this._memGot
       .delete(url, {
         headers: {
           Authorization: authToken,
@@ -58,7 +61,7 @@ export class GotClient implements GotClientType {
     return this._gotResponse;
   }
   async put(url: string, authToken: string): Promise<GotResponse> {
-    this._gotResponse.data = await got
+    this._gotResponse.data = await this._memGot
       .put(url, {
         headers: {
           Authorization: authToken,
@@ -71,7 +74,7 @@ export class GotClient implements GotClientType {
   }
 
   async get(url: string, authToken: string): Promise<GotResponse> {
-    this._gotResponse.data = await got
+    this._gotResponse.data = await this._memGot
       .get(url, {
         headers: {
           Authorization: authToken,
