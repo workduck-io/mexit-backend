33c0db76ec05da2d379fad5fc6fb2210e71351a4
Rishivikram N <rishi.vikram.1@gmail.com>
2022-01-28 14:31:31 +0530

    Added Caching Module

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -4,6 +4,7 @@ import { NodeManager } from './managers/NodeManager';
 import { AuthManager } from './managers/AuthManager';
 import { GotClient } from './libs/GotClientClass';
 import { Transformer } from './libs/TransformerClass';
+import { Cache } from './libs/CacheClass';
 
 const container = new Container();
 
@@ -11,5 +12,5 @@ container.bind<GotClient>(GotClient).to(GotClient).inSingletonScope();
 container.bind<Transformer>(Transformer).to(Transformer).inSingletonScope();
 container.bind<NodeManager>(NodeManager).to(NodeManager).inSingletonScope();
 container.bind<AuthManager>(AuthManager).to(AuthManager).inSingletonScope();
-
+container.bind<Cache>(Cache).to(Cache).inSingletonScope();
 export default container;
