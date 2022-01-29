d21d6564ac0aaeac82cfe721fc6c4b7c38f0a7b0
Rishivikram Nandakumar <rishi.vikram.1@gmail.com>
2022-01-29 13:24:04 +0530

    [TASK] Add GET endpoint for the nodes that have user created so far. (#11)
    https://github.com/workduck-io/mex-browser-extension-backend/pull/11
    
    * Added GET all nodes endpoint
    
    * rebased with main

--- src/managers/NodeManager.ts
+++ src/managers/NodeManager.ts
@@ -7,11 +7,13 @@ import { statusCodes } from '../libs/statusCodes';
 import { GotClient } from '../libs/GotClientClass';
 import container from '../inversify.config';
 import { GotResponse } from '../interfaces/GotClient';
+import { Cache } from '../libs/CacheClass';
 
 @injectable()
 export class NodeManager {
   private _urlPath = '/node';
   public _gotClient: GotClient = container.get<GotClient>(GotClient);
+  public _cache: Cache = container.get<Cache>(Cache);
   async createNode(
     nodeDetail: NodeDetail,
     authToken: string
@@ -143,6 +145,39 @@ export class NodeManager {
         authToken
       );
       return response;
+    } catch (error) {
+      errorlib({
+        message: error.message,
+        errorCode: errorCodes.UNKNOWN,
+        errorObject: error,
+        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
+        metaData: error.message,
+      });
+    }
+  }
+  async getAllNodes(userId: string, authToken: string): Promise<GotResponse> {
+    try {
+      const response = await this._cache.get(userId, () =>
+        this._gotClient.get(
+          `${ConfigService.MEX_BACKEND_URL}${this._urlPath}/all/${userId}`,
+          authToken
+        )
+      );
+      return response;
+    } catch (error) {
+      errorlib({
+        message: error.message,
+        errorCode: errorCodes.UNKNOWN,
+        errorObject: error,
+        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
+        metaData: error.message,
+      });
+    }
+  }
+
+  clearCache(): void {
+    try {
+      this._cache.flush();
     } catch (error) {
       errorlib({
         message: error.message,
