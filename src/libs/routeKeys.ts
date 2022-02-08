269710d89a4310cd6b6eb5c914d94204a086248d
Rishivikram Nandakumar <rishi.vikram.1@gmail.com>
2022-02-08 16:54:38 +0530

    Added GET endpoint for the get node with id and added the cache key combo for unique cache keys (#16)
    https://github.com/workduck-io/mex-browser-extension-backend/pull/16

--- src/libs/routeKeys.ts
+++ src/libs/routeKeys.ts
@@ -1,6 +1,7 @@
 export const RouteKeys = {
   createNode: 'POST /node',
   getAllNodes: `GET /node/all/{id}`,
+  getNode: 'GET /node/{id}',
   appendNode: 'POST /node/{id}/append',
   editBlock: 'POST /node/{id}/blockUpdate',
 };
