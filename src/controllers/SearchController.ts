ef9a5e92d21efa385563b46efebe9ee24b0549ec
Mukul Mehta <hello@mukul-mehta.in>
2022-02-15 04:58:20 +0530

    Remove index operations from node controller

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import crypto from 'crypto';
 import { MeiliSearch, Index, SearchResponse } from 'meilisearch';
 
