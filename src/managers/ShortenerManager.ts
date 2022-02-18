41fe9551b37b692571f59b41f539cde2423abc88
Mukul Mehta <hello@mukul-mehta.in>
2022-02-18 17:06:09 +0530

    Add handler for creating new short

--- src/managers/ShortenerManager.ts
+++ src/managers/ShortenerManager.ts
@@ -1,16 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { injectable } from 'inversify';
+
 import { errorlib } from '../libs/errorlib';
 import { errorCodes } from '../libs/errorCodes';
 import { statusCodes } from '../libs/statusCodes';
 import container from '../inversify.config';
 import { Lambda, InvocationType } from '../libs/LambdaClass';
 import { RouteKeys } from '../libs/routeKeys';
-
+import { LinkNode } from '../interfaces/Node';
 @injectable()
 export class ShortenerManager {
   private _lambdaInvocationType: InvocationType = 'RequestResponse';
   private _getShortsLambdaFunctionName = 'mex-integration-dev-namespaceDetails';
+  private _createShortLambdaFunctionName = 'mex-integration-dev-shorten';
 
   private _lambda: Lambda = container.get<Lambda>(Lambda);
 
@@ -24,6 +26,30 @@ export class ShortenerManager {
           pathParameters: { namespace: namespace },
         }
       );
+      return result.body;
+    } catch (error) {
+      errorlib({
+        message: error.message,
+        errorCode: errorCodes.UNKNOWN,
+        errorObject: error,
+        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
+        metaData: error.message,
+      });
+    }
+  }
+
+  async createNewShort(data: LinkNode): Promise<any> {
+    try {
+      const result = await this._lambda.invoke(
+        this._createShortLambdaFunctionName,
+        this._lambdaInvocationType,
+        {
+          routeKey: RouteKeys.shorten,
+          payload: data,
+        },
+        true
+      );
+
       return result.body;
     } catch (error) {
       errorlib({
