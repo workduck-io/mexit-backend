f206ce2d6f67a8139ea06cb5392cec3aa26f4e07
Mukul Mehta <hello@mukul-mehta.in>
2022-02-19 05:10:30 +0530

    Move URL Shortener to Middleware via direct lambda invocation (#20)
    https://github.com/workduck-io/mexit-backend/pull/20
    
    * Locally invoke shortener lambda to get all shorts by namespace
    
    * Add handler for creating new short
    
    * Refactor node capture endpoint with switch case
    
    * Add link capture endpoint
    
    * Rename getShorts to getStats in manager
    
    * Add deserializer to get node endpoint
    
    * Fix structure of contents when appending to activity node in link capture

--- src/controllers/NodeController.ts
+++ src/controllers/NodeController.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { nanoid } from 'nanoid';
 
 import container from '../inversify.config';
 import { NodeManager } from '../managers/NodeManager';
@@ -8,11 +9,14 @@ import { AuthRequest } from '../middlewares/authrequest';
 import { Transformer } from '../libs/TransformerClass';
 import { NodeResponse } from '../interfaces/Response';
 import { serializeContent } from '../libs/serialize';
+import { ShortenerManager } from '../managers/ShortenerManager';
 
 class NodeController {
   public _urlPath = '/node';
   public _router = express.Router();
   public _nodeManager: NodeManager = container.get<NodeManager>(NodeManager);
+  public _shortenerManager: ShortenerManager =
+    container.get<ShortenerManager>(ShortenerManager);
   public _transformer: Transformer = container.get<Transformer>(Transformer);
 
   constructor() {
@@ -21,6 +25,7 @@ class NodeController {
 
   public initializeRoutes(): void {
     this._router.post(this._urlPath, [AuthRequest], this.createNode);
+
     this._router.post(
       `${this._urlPath}/:nodeId/append`,
       [AuthRequest],
@@ -34,26 +39,31 @@ class NodeController {
     );
 
     this._router.post(
+      `${this._urlPath}/capture/link`,
+      [AuthRequest],
+      this.createLinkCapture
+    );
+
+    this._router.post(
       `${this._urlPath}/:nodeId/blockUpdate`,
       [AuthRequest],
       this.editBlockInNode
     );
-    this._router.post(
-      `${this._urlPath}/link`,
-      [AuthRequest],
-      this.createLinkNode
-    );
+
     this._router.post(
       `${this._urlPath}/content`,
       [AuthRequest],
       this.createContentNode
     );
+
     this._router.get(`${this._urlPath}/:nodeId`, [AuthRequest], this.getNode);
+
     this._router.get(
       `${this._urlPath}/all/:userId`,
       [AuthRequest],
       this.getAllNodes
     );
+
     this._router.get(
       `${this._urlPath}/clearcache`,
       [AuthRequest],
@@ -67,60 +77,158 @@ class NodeController {
       const reqBody = new RequestClass(request, 'ContentNodeRequest').data;
       const type = reqBody.type;
 
-      // Adding to Activity Node
+      switch (type) {
+        case 'DRAFT': {
+          const activityNodeUID = reqBody.id;
+
+          const activityNodeDetail = {
+            type: 'ElementRequest',
+            elements: serializeContent(reqBody.content),
+          };
+
+          activityNodeDetail.elements.forEach(e => {
+            e.createdBy = response.locals.userEmail;
+          });
+
+          const result = await this._nodeManager.appendNode(
+            activityNodeUID,
+            activityNodeDetail
+          );
+          response.json(JSON.parse(result));
+          break;
+        }
+
+        case 'HIERARCHY': {
+          const activityNodeUID = reqBody.id;
+
+          const activityNodeDetail = {
+            type: 'ElementRequest',
+            elements: serializeContent(reqBody.content),
+          };
+
+          activityNodeDetail.elements.forEach(e => {
+            e.createdBy = response.locals.userEmail;
+          });
+
+          const activityPromise = this._nodeManager.appendNode(
+            activityNodeUID,
+            activityNodeDetail
+          );
+
+          let hierarchyPromise: Promise<string>;
+
+          if (reqBody.createNodeUID) {
+            const nodeDetail = {
+              id: reqBody.createNodeUID,
+              // nodePath: reqBody.nodePath,
+              type: 'NodeRequest',
+              lastEditedBy: response.locals.userEmail,
+              namespaceIdentifier: 'NAMESPACE1',
+              workspaceIdentifier: reqBody.workspaceIdentifier,
+              data: serializeContent(reqBody.content),
+              metadata: reqBody.metadata,
+            };
+
+            hierarchyPromise = this._nodeManager.createNode(nodeDetail);
+          } else if (reqBody.appendNodeUID) {
+            const appendDetail = {
+              type: 'ElementRequest',
+              elements: serializeContent(reqBody.content),
+            };
+
+            appendDetail.elements.forEach(e => {
+              e.createdBy = response.locals.userEmail;
+            });
+
+            hierarchyPromise = this._nodeManager.appendNode(
+              reqBody.appendNodeUID,
+              appendDetail
+            );
+          }
+
+          const rawResults = await Promise.all([
+            activityPromise,
+            hierarchyPromise,
+          ]);
+
+          const resp = rawResults.map(resp => {
+            return resp ? JSON.parse(resp) : {};
+          });
+
+          response.json(resp);
+          break;
+        }
+      }
+    } catch (error) {
+      response.status(statusCodes.INTERNAL_SERVER_ERROR).send(error);
+    }
+  };
+
+  createLinkCapture = async (
+    request: Request,
+    response: Response
+  ): Promise<void> => {
+    try {
+      const reqBody = new RequestClass(request, 'LinkCapture').data;
       const activityNodeUID = reqBody.id;
+
+      delete reqBody.id;
+      const shortenerResp = await this._shortenerManager.createNewShort(
+        reqBody
+      );
+      const shortenedURL = JSON.parse(shortenerResp).message;
+
+      if (shortenedURL === 'URL already exists') {
+        response.status(statusCodes.BAD_REQUEST).json({
+          message: 'Alias Already Exists',
+        });
+        return;
+      }
 
       const activityNodeDetail = {
         type: 'ElementRequest',
-        elements: serializeContent(reqBody.content),
+        elements: [
+          {
+            id: `BLOCK_${nanoid()}`,
+            elementType: 'h1',
+            createdBy: response.locals.userEmail,
+            children: [{ id: `TEMP_${nanoid()}`, content: shortenedURL }],
+          },
+          {
+            id: `BLOCK_${nanoid()}`,
+            elementType: 'p',
+            createdBy: response.locals.userEmail,
+            children: [
+              {
+                id: `TEMP_${nanoid()}`,
+                content: '',
+              },
+              {
+                id: `TEMP_${nanoid()}`,
+                elementType: 'a',
+                properties: {
+                  url: reqBody.long,
+                },
+                children: [
+                  {
+                    id: `TEMP_${nanoid()}`,
+                    content: reqBody.long,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
       };
-
-      activityNodeDetail.elements.forEach(e => {
-        e.createdBy = response.locals.userEmail;
-      });
 
       const result = await this._nodeManager.appendNode(
         activityNodeUID,
         activityNodeDetail
       );
 
-      // Add `appendNodeUID` check here to append instead of creating if node already exists
-      if (type === 'HIERARCHY') {
-        if (reqBody.createNodeUID) {
-          const nodeDetail = {
-            id: reqBody.createNodeUID,
-            // nodePath: reqBody.nodePath,
-            type: 'NodeRequest',
-            lastEditedBy: response.locals.userEmail,
-            namespaceIdentifier: 'NAMESPACE1',
-            workspaceIdentifier: reqBody.workspaceIdentifier,
-            data: serializeContent(reqBody.content),
-            metadata: reqBody.metadata,
-          };
-
-          const nodeResult = await this._nodeManager.createNode(nodeDetail);
-
-          response.json(JSON.parse(nodeResult));
-        } else if (reqBody.appendNodeUID) {
-          const appendDetail = {
-            type: 'ElementRequest',
-            elements: serializeContent(reqBody.content),
-          };
-
-          appendDetail.elements.forEach(e => {
-            e.createdBy = response.locals.userEmail;
-          });
-
-          const result = await this._nodeManager.appendNode(
-            reqBody.appendNodeUID,
-            appendDetail
-          );
-
-          response.json(JSON.parse(result));
-        }
-      } else if (type === 'DRAFT') {
-        response.json(JSON.parse(result));
-      }
+      const resp = JSON.parse(result);
+      resp.shortenedURL = shortenedURL;
+      response.json(resp);
     } catch (error) {
       response.status(statusCodes.INTERNAL_SERVER_ERROR).send(error);
     }
@@ -189,28 +297,6 @@ class NodeController {
     }
   };
 
-  createLinkNode = async (
-    request: Request,
-    response: Response
-  ): Promise<void> => {
-    try {
-      const requestDetail = new RequestClass(request, 'LinkNodeRequest');
-      const nodeDetail = this._transformer.convertLinkToNodeFormat(
-        requestDetail.data
-      );
-      const resultNodeDetail = await this._nodeManager.createNode(nodeDetail);
-      const resultLinkNodeDetail = this._transformer.convertNodeToLinkFormat(
-        JSON.parse(resultNodeDetail) as NodeResponse
-      );
-      response
-        .contentType('application/json')
-        .status(statusCodes.OK)
-        .send(resultLinkNodeDetail);
-    } catch (error) {
-      response.status(statusCodes.INTERNAL_SERVER_ERROR).send(error);
-    }
-  };
-
   createContentNode = async (
     request: Request,
     response: Response
@@ -251,6 +337,7 @@ class NodeController {
       const nodeResponse = JSON.parse(result) as NodeResponse;
       const convertedResponse =
         this._transformer.genericNodeConverter(nodeResponse);
+
       response
         .contentType('application/json')
         .status(statusCodes.OK)
---
+++ src/managers/ShortenerManager.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { injectable } from 'inversify';
+
+import { errorlib } from '../libs/errorlib';
+import { errorCodes } from '../libs/errorCodes';
+import { statusCodes } from '../libs/statusCodes';
+import container from '../inversify.config';
+import { Lambda, InvocationType } from '../libs/LambdaClass';
+import { RouteKeys } from '../libs/routeKeys';
+import { LinkCapture } from '../interfaces/Node';
+@injectable()
+export class ShortenerManager {
+  private _lambdaInvocationType: InvocationType = 'RequestResponse';
+  private _getShortsLambdaFunctionName = 'mex-integration-dev-namespaceDetails';
+  private _createShortLambdaFunctionName = 'mex-integration-dev-shorten';
+
+  private _lambda: Lambda = container.get<Lambda>(Lambda);
+
+  async getStatsByWorkspace(namespace: string): Promise<any> {
+    try {
+      const result = await this._lambda.invoke(
+        this._getShortsLambdaFunctionName,
+        this._lambdaInvocationType,
+        {
+          routeKey: RouteKeys.getShorts,
+          pathParameters: { namespace: namespace },
+        }
+      );
+      return result.body;
+    } catch (error) {
+      errorlib({
+        message: error.message,
+        errorCode: errorCodes.UNKNOWN,
+        errorObject: error,
+        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
+        metaData: error.message,
+      });
+    }
+  }
+
+  async createNewShort(data: LinkCapture): Promise<any> {
+    try {
+      const result = await this._lambda.invoke(
+        this._createShortLambdaFunctionName,
+        this._lambdaInvocationType,
+        {
+          routeKey: RouteKeys.shorten,
+          payload: data,
+        },
+        true
+      );
+
+      return result.body;
+    } catch (error) {
+      errorlib({
+        message: error.message,
+        errorCode: errorCodes.UNKNOWN,
+        errorObject: error,
+        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
+        metaData: error.message,
+      });
+    }
+  }
+}
