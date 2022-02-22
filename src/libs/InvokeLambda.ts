4a6c6ad40091056d6bc72002c3ca6050732f5be6
Rishivikram N <rishi.vikram.1@gmail.com>
2022-02-22 18:50:21 +0530

    Comment local env changes

--- src/libs/InvokeLambda.ts
+++ src/libs/InvokeLambda.ts
@@ -2,11 +2,11 @@ import LambdaClient from 'aws-sdk/clients/lambda';
 
 const lambdaClient = new LambdaClient({
   region: 'us-east-1',
-  endpoint: 'http://localhost:3002',
-  sslEnabled: false,
-  apiVersion: '2015-03-31',
-  accessKeyId: 'any',
-  secretAccessKey: 'any',
+  // endpoint: 'http://localhost:3002',
+  // sslEnabled: false,
+  // apiVersion: '2015-03-31',
+  // accessKeyId: 'any',
+  // secretAccessKey: 'any',
 });
 
 export default {
