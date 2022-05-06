559ecbd37e33602d0633c14a26870586d80cf314
Mukul Mehta <mukul@workduck.io>
2022-05-06 16:07:47 +0530

    Remove DirectLambdaInvocationClass altogether

--- src/libs/LambdaClass.ts
+++ src/libs/LambdaClass.ts
@@ -1,11 +1,12 @@
 import { injectable } from 'inversify';
 import LambdaConfig from './InvokeLambda';
 export type InvocationType = 'RequestResponse' | 'Event';
+export type InvocationSource = 'Direct' | 'APIGateway';
 
 interface LambdaOptions {
   payload?: any;
   pathParameters?: any;
-  routeKey: string;
+  routeKey?: string;
   queryStringParameters?: any;
   headers?: any;
 }
@@ -16,48 +17,34 @@ export class Lambda {
     functionName: string,
     invocationType: InvocationType,
     options: LambdaOptions,
-    sendRawBody = false
+    sendRawBody = false,
+    invocationSource: InvocationSource = 'APIGateway'
   ) {
     return JSON.parse(
       (
         await LambdaConfig.invokeLambdaClient({
           FunctionName: functionName,
-          Payload: JSON.stringify({
-            ...(options.pathParameters && {
-              pathParameters: options.pathParameters,
-            }),
-            ...(options.payload && {
-              body: sendRawBody
-                ? options.payload
-                : JSON.stringify(options.payload),
-            }),
-            routeKey: options.routeKey,
-            ...(options.queryStringParameters && {
-              queryStringParameters: options.queryStringParameters,
-            }),
-            ...(options.headers && {
-              headers: options.headers,
-            }),
-          }),
-          InvocationType: invocationType,
-        })
-      ).Payload as string
-    );
-  }
-}
-
-@injectable()
-export class DirectLambdaInvocation {
-  async invoke(
-    functionName: string,
-    invocationType: InvocationType,
-    body: any
-  ) {
-    return JSON.parse(
-      (
-        await LambdaConfig.invokeLambdaClient({
-          FunctionName: functionName,
-          Payload: JSON.stringify(body),
+          Payload: JSON.stringify(
+            invocationSource === 'Direct'
+              ? options.payload
+              : {
+                  ...(options.pathParameters && {
+                    pathParameters: options.pathParameters,
+                  }),
+                  ...(options.payload && {
+                    body: sendRawBody
+                      ? options.payload
+                      : JSON.stringify(options.payload),
+                  }),
+                  routeKey: options.routeKey,
+                  ...(options.queryStringParameters && {
+                    queryStringParameters: options.queryStringParameters,
+                  }),
+                  ...(options.headers && {
+                    headers: options.headers,
+                  }),
+                }
+          ),
           InvocationType: invocationType,
         })
       ).Payload as string
