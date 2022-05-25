4b8e596ab8880b6a924f0d7161e03add8c50a5af
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-05-25 21:16:19 +0530

    Fix parent duplication on child node creation (#58)
    https://github.com/workduck-io/mexit-backend/pull/58
    
    * New Link Hierarchy to ILink parser
    
    * Use the new parser when returning ilinks
    
    Co-authored-by: Mukul Mehta <mukul@workduck.io>

--- src/libs/TransformerClass.ts
+++ src/libs/TransformerClass.ts
@@ -9,11 +9,51 @@ import { deserializeContent } from './serialize';
 export class Transformer {
   private CACHE_KEY_DELIMITER = '+';
   private LINK_HIERARCHY_DELIMITER = '#';
+  private ILINK_DELIMITER = '.';
   private CAPTURES = {
     CONTENT: 'CONTENT',
     LINK: 'LINK',
     NODE: 'NODE',
     SNIPPET: 'SNIPPET',
+  };
+
+  linkHierarchyParser = (linkData: string[]): ILink[] => {
+    const ilinks: ILink[] = [];
+    const idPathMapping: { [key: string]: string } = {};
+
+    for (const subTree of linkData) {
+      const nodes = subTree.split(this.LINK_HIERARCHY_DELIMITER);
+      let prefix: string;
+
+      if (nodes.length % 2 !== 0) throw new Error('Invalid Linkdata Input');
+
+      for (let index = 0; index < nodes.length; index += 2) {
+        const nodeTitle = nodes[index];
+        const nodeID = nodes[index + 1];
+
+        const nodePath = prefix
+          ? `${prefix}${this.ILINK_DELIMITER}${nodeTitle}`
+          : nodeTitle;
+
+        /*
+          Drafts.A and Drafts.B exist, we need to check if the Drafts parent node is the same by checking
+          the parent nodeUID. This handles the case in which a nodeID might have two different node paths. 
+ 
+          We still do not handle the case where there are 2 nodes with the same path but different Node IDs,
+          we handle that on the frontend for now
+        */
+        if (idPathMapping[nodeID]) {
+          if (idPathMapping[nodeID] !== nodePath)
+            throw new Error('Invalid Linkdata Input');
+        } else {
+          idPathMapping[nodeID] = nodePath;
+          ilinks.push({ nodeid: nodeID, path: nodePath });
+        }
+        prefix = nodePath;
+      }
+    }
+
+    return ilinks;
   };
 
   decodeLinkHierarchy = (linkDatas: string[]): Promise<ILink[]> => {
