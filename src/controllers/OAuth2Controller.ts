0d89a0a3760df1fe8c3a6b280607a0994ca25000
Rishivikram N <rishi.vikram.1@gmail.com>
2022-06-01 20:48:01 +0530

    Redirect url updated

--- src/controllers/OAuth2Controller.ts
+++ src/controllers/OAuth2Controller.ts
@@ -77,7 +77,7 @@ class OAuth2Controller {
         .set('Content-Type', 'text/html')
         .send(
           Buffer.from(
-            `<html lang="en"><head><meta charset="UTF-8"><title>Auth Success</title> <script>window.location.href="mex://localhost:3333?access_token=${tokens.access_token}&id_token=${tokens.id_token}&refresh_token=${tokens.refresh_token}&type=calendar_google"; window.close()</script></head><body><p>Please return to the mex app...</p></body></html>`
+            `<html lang="en"><head><meta charset="UTF-8"><title>Auth Success</title> <script>window.location.href="mex://redirect?access_token=${tokens.access_token}&id_token=${tokens.id_token}&refresh_token=${tokens.refresh_token}&type=calendar_google"; window.close()</script></head><body><p>Please return to the mex app...</p></body></html>`
           )
         )
         .status(200);
