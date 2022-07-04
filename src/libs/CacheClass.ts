7217a6c2f32900eeea5bbbe35fb0c847218d0221
Mukul Mehta <mukul@workduck.io>
2022-07-04 16:40:53 +0530

    Add support for returning node metadata along with ilinks (#69)
    https://github.com/workduck-io/mexit-backend/pull/69

--- src/libs/CacheClass.ts
+++ src/libs/CacheClass.ts
@@ -1,6 +1,5 @@
 import { injectable } from 'inversify';
 import NodeCache from 'node-cache';
-import { NodeDataResponse, NodeResponse } from '../interfaces/Response';
 import container from '../inversify.config';
 import { Transformer } from './TransformerClass';
 
@@ -53,49 +52,6 @@ export class Cache {
 
   has(key: string, entity: string) {
     return this._cache.has(this._transformer.encodeCacheKey(entity, key));
-  }
-
-  appendOrCreateActivityNode(
-    userId: string,
-    activityNodeLabel: string,
-    activityNode: NodeResponse,
-    activityBlock: NodeDataResponse
-  ) {
-    //push the latest capture always at the zeroth index
-    const insertIndex = 0;
-    //No need of deleting anything for this feature
-    const deleteCount = 0;
-    const hasKey = this._cache.has(
-      this._transformer.encodeCacheKey(activityNodeLabel, userId)
-    );
-
-    if (hasKey) {
-      //take out from the cache
-      const cachedActivityNode: any = this._cache.take(
-        this._transformer.encodeCacheKey(activityNodeLabel, userId)
-      );
-
-      if (cachedActivityNode.data && cachedActivityNode.data.length > 0) {
-        const poppedBlock = cachedActivityNode.data.pop();
-        // update the endCursor with the popped block
-        cachedActivityNode.endCursor = `${cachedActivityNode.id}$${poppedBlock.id}`;
-      }
-      //update the activitynode
-      cachedActivityNode.data.splice(insertIndex, deleteCount, activityBlock);
-
-      //set the updated block again in the cache
-      this._cache.set(
-        this._transformer.encodeCacheKey(activityNodeLabel, userId),
-        cachedActivityNode
-      );
-      return cachedActivityNode;
-    } else {
-      this._cache.set(
-        this._transformer.encodeCacheKey(activityNodeLabel, userId),
-        activityNode
-      );
-      return activityNode;
-    }
   }
 
   del(key: string, entity: string) {
