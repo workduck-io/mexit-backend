61134747a8e4a02280b63b207d50bbb2ea50aca6
Rishivikram N <rishi.vikram.1@gmail.com>
2022-07-09 17:18:04 +0530

    Added Client GeoIP for the error logging requests too

--- src/middlewares/responseErrorHandler.ts
+++ src/middlewares/responseErrorHandler.ts
@@ -1,6 +1,7 @@
 import { statusCodes } from './../libs/statusCodes';
 import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
 import logger from '../libs/logger';
+import geoip from 'geoip-lite';
 import { WDError } from '@workduck-io/wderror';
 
 const responseErrorHandler: ErrorRequestHandler = (
@@ -12,14 +13,18 @@ const responseErrorHandler: ErrorRequestHandler = (
   const statusCode: number =
     error.response?.statusCode ?? statusCodes.INTERNAL_SERVER_ERROR;
   const message = error.response?.message ?? error.message;
+  const clientIp =
+    request.headers['x-forwarded-for']?.toString().split(',')[0] ||
+    request.socket.remoteAddress;
+  const clientLocation = geoip.lookup(clientIp);
 
   if (statusCode >= 400 && statusCode < 500) {
     logger.warn(
-      `HTTP [${request.method}] ${request.url} - ${statusCode} ${message}`
+      `HTTP [${request.method}] ${request.url} - ${statusCode} ${message} | client IP ${clientIp} | LatLong ${clientLocation?.ll} | ${clientLocation?.city}, ${clientLocation?.region}, ${clientLocation?.country}`
     );
   } else if (statusCode >= 500) {
     logger.error(
-      `HTTP [${request.method}] ${request.url} - ${statusCode} ${message}`
+      `HTTP [${request.method}] ${request.url} - ${statusCode} ${message} | client IP ${clientIp} | LatLong ${clientLocation?.ll} | ${clientLocation?.city}, ${clientLocation?.region}, ${clientLocation?.country}`
     );
     logger.error(`Call Stack ${error.response?.stackTrace ?? error.stack}`);
   }
