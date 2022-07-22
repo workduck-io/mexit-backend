7c5013b02cbb7e0372052eb3460b81a2fa488b8d
Rishivikram N <rishi.vikram.1@gmail.com>
2022-07-22 15:43:28 +0530

    Added workspace id to request logs
    (cherry picked from commit d96dae95c61ff1e9dcf28cdf733a5883dc02ca35)

--- src/middlewares/logrequest.ts
+++ src/middlewares/logrequest.ts
@@ -12,10 +12,16 @@ async function LogRequest(
       req.headers['x-forwarded-for']?.toString().split(',')[0] ||
       req.socket.remoteAddress;
     const clientLocation = geoip.lookup(clientIp);
+    const workspaceId = req.headers['mex-workspace-id'];
 
-    logger.info(
-      `HTTP [${req.method}] ${req.url} | client IP ${clientIp} | LatLong ${clientLocation?.ll} | ${clientLocation?.city}, ${clientLocation?.region}, ${clientLocation?.country}`
-    );
+    if (workspaceId)
+      logger.info(
+        `HTTP [${req.method}] ${req.url} | ${workspaceId} | client IP ${clientIp} | LatLong ${clientLocation?.ll} | ${clientLocation?.city}, ${clientLocation?.region}, ${clientLocation?.country}`
+      );
+    else
+      logger.info(
+        `HTTP [${req.method}] ${req.url} | client IP ${clientIp} | LatLong ${clientLocation?.ll} | ${clientLocation?.city}, ${clientLocation?.region}, ${clientLocation?.country}`
+      );
   }
   next();
 }
