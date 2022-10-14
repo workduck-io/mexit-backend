8b6dbe2953da46d0c53a70d5f1f7f64a03cca699
Mukul Mehta <mukul@workduck.io>
2022-10-14 20:50:16 +0530

    Add endpoints for url shortener lambdas

--- src/managers/LinkManager.ts
+++ src/managers/LinkManager.ts
@@ -5,25 +5,32 @@ import { statusCodes } from '../libs/statusCodes';
 import container from '../inversify.config';
 import { Lambda, InvocationType } from '../libs/LambdaClass';
 import { RouteKeys } from '../libs/routeKeys';
-import { LinkCapture } from '../interfaces/Node';
+import { STAGE } from '../env';
+
 @injectable()
 export class LinkManager {
   private _lambdaInvocationType: InvocationType = 'RequestResponse';
-  private _getShortsLambdaFunctionName = 'mex-integration-dev-namespaceDetails';
-  private _createShortLambdaFunctionName = 'mex-integration-dev-shorten';
+  private _linkServiceLambdaBase = `mex-url-shortner-${STAGE}`;
 
   private _lambda: Lambda = container.get<Lambda>(Lambda);
 
-  async getStatsByWorkspace(namespace: string): Promise<any> {
+  async createNewShort(
+    workspaceID: string,
+    idToken: string,
+    data: any
+  ): Promise<any> {
     try {
       const result = await this._lambda.invokeAndCheck(
-        this._getShortsLambdaFunctionName,
+        `${this._linkServiceLambdaBase}-shorten`,
         this._lambdaInvocationType,
         {
-          routeKey: RouteKeys.getShorts,
-          pathParameters: { namespace: namespace },
-        }
+          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+          httpMethod: 'POST',
+          payload: data,
+        },
+        true
       );
+
       return result;
     } catch (error) {
       errorlib({
@@ -36,17 +43,76 @@ export class LinkManager {
     }
   }
 
-  async createNewShort(data: LinkCapture): Promise<any> {
+  async getAllShortsOfWorkspace(
+    workspaceID: string,
+    idToken: string
+  ): Promise<any> {
     try {
-      const { linksNodeID, ...payload } = data;
       const result = await this._lambda.invokeAndCheck(
-        this._createShortLambdaFunctionName,
+        `${this._linkServiceLambdaBase}-workspaceDetails`,
         this._lambdaInvocationType,
         {
-          routeKey: RouteKeys.shorten,
-          payload: payload,
+          httpMethod: 'GET',
+          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
         },
         true
+      );
+
+      return result;
+    } catch (error) {
+      errorlib({
+        message: error.message,
+        errorCode: error.statusCode,
+        errorObject: error,
+        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
+        metaData: error.message,
+      });
+    }
+  }
+
+  async deleteShort(
+    workspaceID: string,
+    idToken: string,
+    url: string
+  ): Promise<any> {
+    try {
+      const result = await this._lambda.invokeAndCheck(
+        `${this._linkServiceLambdaBase}-del`,
+        this._lambdaInvocationType,
+        {
+          httpMethod: 'DELETE',
+          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+          pathParameters: { url: url },
+        },
+        true
+      );
+
+      return result;
+    } catch (error) {
+      errorlib({
+        message: error.message,
+        errorCode: error.statusCode,
+        errorObject: error,
+        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
+        metaData: error.message,
+      });
+    }
+  }
+
+  async getStatsByURL(
+    workspaceID: string,
+    idToken: string,
+    url: string
+  ): Promise<any> {
+    try {
+      const result = await this._lambda.invokeAndCheck(
+        `${this._linkServiceLambdaBase}-stats`,
+        this._lambdaInvocationType,
+        {
+          httpMethod: 'GET',
+          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+          pathParameters: { url: url },
+        }
       );
 
       return result;
