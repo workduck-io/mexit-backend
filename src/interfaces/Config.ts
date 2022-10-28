39e8f4a0be49a305de8c296fd39f8b65d7525e53
Rishivikram N <rishi.vikram.1@gmail.com>
2022-10-28 15:33:18 +0530

    Added useracesstype in-memory cache

--- src/interfaces/Config.ts
+++ src/interfaces/Config.ts
@@ -2,5 +2,6 @@ export const CacheType = {
   NamespaceHierarchy: Symbol.for('NamespaceHeirarchy'),
   Node: Symbol.for('Node'),
   UserAccess: Symbol.for('UserAccess'),
+  UserAccessType: Symbol.for('UserAccessType'),
   Snippet: Symbol.for('Snippet'),
 };
