92d6502b5a8e76fb1fd599a9518532f79c21782a
Mukul Mehta <mukul@workduck.io>
2022-11-12 19:50:11 +0530

    Change default stage to staging; Add DOCKER_BUILDKIT=1 flag in GH Actions; Fix reversed taskdef stages

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
 export const IS_DEV = process.env.NODE_ENV === 'production' ? false : true;
 export const AWS_REGION = 'us-east-1';
-export const STAGE = process.env.STAGE ?? 'test';
+export const STAGE = process.env.STAGE ?? 'staging';
+
+console.log('STARTING WITH STAGE: ', STAGE);
