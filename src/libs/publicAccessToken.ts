40a4297d54c6cd70e2330b14e5863e7efe305bc2
Mukul Mehta <mukul@workduck.io>
2023-01-27 14:16:29 +0530

    Get cognito pool and client ID from stage

--- src/libs/publicAccessToken.ts
+++ src/libs/publicAccessToken.ts
@@ -1,4 +1,5 @@
 import got from 'got';
+import { COGNITO_CLIENT_ID } from '../env';
 
 export interface PublicAccessCreds {
   idToken?: string;
@@ -8,8 +9,6 @@ export interface PublicAccessCreds {
 }
 
 const REFRESH_TOKEN = process.env.MEX_DEFAULT_USER_REFRESH_TOKEN;
-const CLIENT_ID = process.env.MEXIT_BACKEND_CLIENT_ID;
-
 export const initialPublicCreds = (): PublicAccessCreds => {
   return { refreshToken: REFRESH_TOKEN };
 };
@@ -23,7 +22,7 @@ export const refreshAccessCreds = async (
     .post(URL, {
       form: {
         grant_type: 'refresh_token',
-        client_id: CLIENT_ID,
+        client_id: COGNITO_CLIENT_ID,
         refresh_token: REFRESH_TOKEN,
       },
     })
