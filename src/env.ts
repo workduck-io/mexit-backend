81b0cdc5162699c70c6de3e84bd4d6da7d2556fe
Mukul Mehta <mukul@workduck.io>
2023-01-27 14:16:29 +0530

    Get cognito pool and client ID from stage

--- src/env.ts
+++ src/env.ts
@@ -2,4 +2,20 @@ export const IS_DEV = process.env.NODE_ENV === 'production' ? false : true;
 export const AWS_REGION = 'us-east-1';
 export const STAGE = process.env.STAGE ?? 'staging';
 
+const COGNITO_POOL_ID_MAP = {
+  staging: 'us-east-1_Zu7FAh7hj',
+  test: 'us-east-1_Zu7FAh7hj',
+};
+
+const COGNITO_CLIENT_ID_MAP = {
+  staging: '6pvqt64p0l2kqkk2qafgdh13qe',
+  test: '25qd6eq6vv3906osgv8v3f8c6v',
+};
+
+export const COGNITO_POOL_ID =
+  process.env.COGNITO_POOL_ID ?? COGNITO_POOL_ID_MAP[STAGE];
+
+export const COGNITO_CLIENT_ID =
+  process.env.MEXIT_BACKEND_CLIENT_ID ?? COGNITO_CLIENT_ID_MAP[STAGE];
+
 console.log('STARTING WITH STAGE: ', STAGE);
