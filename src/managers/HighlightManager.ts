9a6d02dc6d6ff869ce4ec5a48f37fa9bef406e65
rpPanda <ruddhi@workduck.io>
2023-02-01 14:27:01 +0530

    Moved to BubbleUnexpectedError decorator for Manager error handling

--- src/managers/HighlightManager.ts
+++ src/managers/HighlightManager.ts
@@ -2,10 +2,9 @@ import { injectable } from 'inversify';
 
 import { STAGE } from '../env';
 import container from '../inversify.config';
-import { errorlib } from '../libs/errorlib';
 import { InvocationType, Lambda } from '../libs/LambdaClass';
 import { RouteKeys } from '../libs/routeKeys';
-import { statusCodes } from '../libs/statusCodes';
+import { BubbleUnexpectedError } from '../utils/decorators';
 
 @injectable()
 export class HighlightManager {
@@ -14,121 +13,85 @@ export class HighlightManager {
 
   private _lambda: Lambda = container.get<Lambda>(Lambda);
 
+  @BubbleUnexpectedError()
   async createHighlight(
     workspaceID: string,
     idToken: string,
     data: any
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._highlightServiceLambdaName,
-        this._lambdaInvocationType,
-        {
-          routeKey: RouteKeys.createHighlight,
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          httpMethod: 'POST',
-          payload: data,
-        },
-        true
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._highlightServiceLambdaName,
+      this._lambdaInvocationType,
+      {
+        routeKey: RouteKeys.createHighlight,
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        httpMethod: 'POST',
+        payload: data,
+      },
+      true
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 
+  @BubbleUnexpectedError()
   async getMultipleHighlights(
     workspaceID: string,
     idToken: string,
     ids: string[]
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._highlightServiceLambdaName,
-        this._lambdaInvocationType,
-        {
-          routeKey: RouteKeys.getHighlightByIDs,
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          httpMethod: 'POST',
-          payload: { ids },
-        },
-        true
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._highlightServiceLambdaName,
+      this._lambdaInvocationType,
+      {
+        routeKey: RouteKeys.getHighlightByIDs,
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        httpMethod: 'POST',
+        payload: { ids },
+      },
+      true
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 
+  @BubbleUnexpectedError()
   async getAllHighlightsOfWorkspace(
     workspaceID: string,
     idToken: string
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._highlightServiceLambdaName,
-        this._lambdaInvocationType,
-        {
-          routeKey: RouteKeys.getAllHighlightsOfWorkspace,
-          httpMethod: 'GET',
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-        },
-        true
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._highlightServiceLambdaName,
+      this._lambdaInvocationType,
+      {
+        routeKey: RouteKeys.getAllHighlightsOfWorkspace,
+        httpMethod: 'GET',
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+      },
+      true
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 
+  @BubbleUnexpectedError()
   async deleteHighlight(
     workspaceID: string,
     idToken: string,
     id: string
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._highlightServiceLambdaName,
-        this._lambdaInvocationType,
-        {
-          routeKey: RouteKeys.deleteHighlightByID,
-          httpMethod: 'DELETE',
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          pathParameters: { entityId: id },
-        },
-        true
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._highlightServiceLambdaName,
+      this._lambdaInvocationType,
+      {
+        routeKey: RouteKeys.deleteHighlightByID,
+        httpMethod: 'DELETE',
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        pathParameters: { entityId: id },
+      },
+      true
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 }
