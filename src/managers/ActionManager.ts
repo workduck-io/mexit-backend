fd50b4757aa453602793f6cb8d4329323bf23229
rpPanda <ruddhi@workduck.io>
2023-02-01 15:43:37 +0530

    Used error decorator

--- src/managers/ActionManager.ts
+++ src/managers/ActionManager.ts
@@ -2,10 +2,9 @@ import { injectable } from 'inversify';
 
 import { STAGE } from '../env';
 import container from '../inversify.config';
-import { errorlib } from '../libs/errorlib';
 import { InvocationType, Lambda } from '../libs/LambdaClass';
 import { RouteKeys } from '../libs/routeKeys';
-import { statusCodes } from '../libs/statusCodes';
+import { BubbleUnexpectedError } from '../utils/decorators';
 
 @injectable()
 export class ActionManager {
@@ -26,175 +25,121 @@ export class ActionManager {
 
   private _lambda: Lambda = container.get<Lambda>(Lambda);
 
+  @BubbleUnexpectedError()
   async getAction(
     workspaceID: string,
     idToken: string,
     actionGroupId: string,
     actionId: string
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._lambdaName.action.get,
-        this._lambdaInvocationType,
-        {
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          httpMethod: 'GET',
-          pathParameters: { actionGroupId, actionId },
-          routeKey: RouteKeys.getAction,
-        }
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._lambdaName.action.get,
+      this._lambdaInvocationType,
+      {
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        httpMethod: 'GET',
+        pathParameters: { actionGroupId, actionId },
+        routeKey: RouteKeys.getAction,
+      }
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 
+  @BubbleUnexpectedError()
   async getAllActions(
     workspaceID: string,
     idToken: string,
     actionGroupId: string
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._lambdaName.action.getAll,
-        this._lambdaInvocationType,
-        {
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          httpMethod: 'GET',
-          pathParameters: { actionGroupId },
-          routeKey: RouteKeys.getActionsOfActionGroup,
-        }
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._lambdaName.action.getAll,
+      this._lambdaInvocationType,
+      {
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        httpMethod: 'GET',
+        pathParameters: { actionGroupId },
+        routeKey: RouteKeys.getActionsOfActionGroup,
+      }
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 
+  @BubbleUnexpectedError()
   async getAuth(
     workspaceID: string,
     idToken: string,
     authTypeId: string
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._lambdaName.auth.get,
-        this._lambdaInvocationType,
-        {
-          httpMethod: 'GET',
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          pathParameters: { authTypeId },
-          routeKey: RouteKeys.getAuth,
-        }
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._lambdaName.auth.get,
+      this._lambdaInvocationType,
+      {
+        httpMethod: 'GET',
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        pathParameters: { authTypeId },
+        routeKey: RouteKeys.getAuth,
+      }
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 
+  @BubbleUnexpectedError()
   async getAllAuths(workspaceID: string, idToken: string): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._lambdaName.auth.getAll,
-        this._lambdaInvocationType,
-        {
-          httpMethod: 'GET',
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          routeKey: RouteKeys.getAllAuths,
-        }
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._lambdaName.auth.getAll,
+      this._lambdaInvocationType,
+      {
+        httpMethod: 'GET',
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        routeKey: RouteKeys.getAllAuths,
+      }
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 
+  @BubbleUnexpectedError()
   async updateAuth(
     workspaceID: string,
     idToken: string,
     authTypeId: string,
     data: any
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._lambdaName.auth.update,
-        this._lambdaInvocationType,
-        {
-          httpMethod: 'GET',
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          pathParameters: { authTypeId },
-          payload: data,
-          routeKey: RouteKeys.updateAuth,
-        }
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._lambdaName.auth.update,
+      this._lambdaInvocationType,
+      {
+        httpMethod: 'GET',
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        pathParameters: { authTypeId },
+        payload: data,
+        routeKey: RouteKeys.updateAuth,
+      }
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 
+  @BubbleUnexpectedError()
   async refreshAuth(
     workspaceID: string,
     idToken: string,
     source: string
   ): Promise<any> {
-    try {
-      const result = await this._lambda.invokeAndCheck(
-        this._lambdaName.auth.refresh,
-        this._lambdaInvocationType,
-        {
-          httpMethod: 'GET',
-          headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
-          pathParameters: { source },
-          routeKey: RouteKeys.refreshAuth,
-        }
-      );
+    const result = await this._lambda.invokeAndCheck(
+      this._lambdaName.auth.refresh,
+      this._lambdaInvocationType,
+      {
+        httpMethod: 'GET',
+        headers: { 'mex-workspace-id': workspaceID, authorization: idToken },
+        pathParameters: { source },
+        routeKey: RouteKeys.refreshAuth,
+      }
+    );
 
-      return result;
-    } catch (error) {
-      errorlib({
-        message: error.message,
-        errorCode: error.statusCode,
-        errorObject: error,
-        statusCode: statusCodes.INTERNAL_SERVER_ERROR,
-        metaData: error.message,
-      });
-    }
+    return result;
   }
 }
