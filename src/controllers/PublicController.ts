7046b1d74837a3e3b47f8e98d6c78b736762f2dc
Mukul Mehta <mukul@workduck.io>
2023-02-18 22:39:00 +0530

    [HOTFIX] Cache public notes namespaces directly (#150)
    https://github.com/workduck-io/mexit-backend/pull/150

--- src/controllers/PublicController.ts
+++ src/controllers/PublicController.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 
 import { STAGE } from '../env';
 import container from '../inversify.config';
+import { Redis } from '../libs/RedisClass';
 import { statusCodes } from '../libs/statusCodes';
 import { Transformer } from '../libs/TransformerClass';
 import { initializePublicRoutes } from '../routes/PublicRoutes';
@@ -9,8 +10,9 @@ import { initializePublicRoutes } from '../routes/PublicRoutes';
 class PublicController {
   public _urlPath = '/public';
   public _router = express.Router();
-  public _transformer: Transformer = container.get<Transformer>(Transformer);
 
+  private _transformer: Transformer = container.get<Transformer>(Transformer);
+  private _cache: Redis = container.get<Redis>(Redis);
   private _nsLambdaFunctionName = `mex-backend-${STAGE}-Namespace:latest`;
   private _nodeLambdaFunctionName = `mex-backend-${STAGE}-Node:latest`;
 
@@ -21,9 +23,15 @@ class PublicController {
   getPublicNode = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
     try {
       const nodeId = request.params.nodeId;
-      const result = await response.locals.invoker(this._nodeLambdaFunctionName, 'getPublicNode', {
-        pathParameters: { id: nodeId },
-      });
+      const result = await this._cache.getOrSet(
+        {
+          key: nodeId,
+        },
+        () =>
+          response.locals.invoker(this._nodeLambdaFunctionName, 'getPublicNode', {
+            pathParameters: { id: nodeId },
+          })
+      );
 
       response.status(statusCodes.OK).json(result);
     } catch (error) {
@@ -33,11 +41,18 @@ class PublicController {
 
   getPublicNamespace = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
     try {
-      const publicNamespace = await response.locals.invoker(this._nsLambdaFunctionName, 'getPublicNamespace', {
-        pathParameters: { id: request.params.namespaceID },
-      });
-
-      const parsedPublicNS = this._transformer.namespaceHierarchyParser(publicNamespace);
+      const namespaceID = request.params.namespaceID;
+      const parsedPublicNS = await this._cache.getOrSet(
+        {
+          key: namespaceID,
+        },
+        () =>
+          response.locals
+            .invoker(this._nsLambdaFunctionName, 'getPublicNamespace', {
+              pathParameters: { id: request.params.namespaceID },
+            })
+            .then(res => this._transformer.namespaceHierarchyParser(res))
+      );
       response.status(statusCodes.OK).json(parsedPublicNS);
     } catch (error) {
       next(error);
