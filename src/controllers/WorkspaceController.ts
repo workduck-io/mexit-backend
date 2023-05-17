15ae096fd48175f40fa797ce72176895b73c58f3
Dinesh Singh <dinesh@workduck.io>
2023-05-17 13:15:49 +0530

    Added Get And Update Workspace

--- src/controllers/WorkspaceController.ts
+++ src/controllers/WorkspaceController.ts
@@ -20,39 +20,50 @@ class WorkspaceController {
   }
 
   updateWorkspace = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
-    const userSpecificKey = this._transformer.encodeCacheKey(
-      this._WorkspaceLabel,
-      response.locals.userId,
-    );
+    const userSpecificKey = this._transformer.encodeCacheKey(this._WorkspaceLabel, response.locals.userId);
     const data = new RequestClass(request).data;
     try {
-      const result = await response.locals.invoker('updateWorkspace', {
-        payload: { ...data, 'type': 'WorkspaceRequest' }
+      await response.locals.invoker('updateWorkspace', {
+        payload: { ...data, type: 'WorkspaceRequest' },
       });
-      await this._redisCache.del(userSpecificKey)
-      response.status(statusCodes.OK).json(result);
+
+      await this._redisCache.del(userSpecificKey);
+
+      response.status(statusCodes.NO_CONTENT).send();
     } catch (error) {
       next(error);
     }
   };
 
+  getWorkspaceByIds = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+    const data = new RequestClass(request).data;
+    const ids = data.ids;
+
+    try {
+      const result = await response.locals.invoker('getWorkspaceByIds', {
+        pathParameters: {
+          ids: ids?.join(','),
+        },
+      });
+
+      response.status(statusCodes.OK).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
 
   getAllWorkspacesOfUser = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
     try {
-      const userSpecificKey = this._transformer.encodeCacheKey(
-        this._WorkspaceLabel,
-        response.locals.userId,
-      );
+      const userSpecificKey = this._transformer.encodeCacheKey(this._WorkspaceLabel, response.locals.userId);
       const result = await this._redisCache.getOrSet({ key: userSpecificKey }, () => {
         return response.locals.invoker('getAllWorkspaceOfUser');
-      })
+      });
 
       response.status(statusCodes.OK).json(result);
     } catch (error) {
       next(error);
     }
   };
-
 }
 
 export default WorkspaceController;
